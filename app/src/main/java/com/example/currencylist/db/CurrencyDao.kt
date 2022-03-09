1a0535fb9f29cc30cc5853391aa6854f4dd2fe68
Man <lcws041129@gmail.com>
2022-03-09 15:51:19 +0800

    add sc and sort by name

--- app/src/main/java/com/example/currencylist/db/CurrencyDao.kt
+++ app/src/main/java/com/example/currencylist/db/CurrencyDao.kt
@@ -12,10 +12,10 @@ interface CurrencyDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insert(item: CurrencyEntity): Long
 
-    @Query("SELECT id, name, symbol FROM currency order by id asc")
+    @Query("SELECT id, name, symbol FROM currency order by name asc")
     fun getAll(): List<CurrencyModel>
 
-    @Query("SELECT id, name, symbol FROM currency order by id desc")
+    @Query("SELECT id, name, symbol FROM currency order by name desc")
     fun getAllDesc(): List<CurrencyModel>
 
     @Query("SELECT COUNT(*) FROM currency")
